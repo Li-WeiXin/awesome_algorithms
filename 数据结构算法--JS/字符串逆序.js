0ca2358ba0ba05157dc4119d3bb22d5108ba27c0
Liweixin <2522048902@qq.com>
2021-02-02 19:45:18 +0800

    字符串逆序

--- 数据结构算法--JS/字符串逆序.js
+++ 数据结构算法--JS/字符串逆序.js
@@ -1,3 +1,18 @@
 //方法一：split结合 ==> reverse 先转换成数组，再将数组拼接成字符串
 var str = 'abc';
-var reverseStr = str.split('').reverse().join('')
+var reverseStr = str.split('').reverse().join('');
+
+// 经过此操作str不变
+// 方法二：不使用API
+var x = 501120;
+var digit;
+var ret = 0;
+var count = [];
+while(x>0) {
+  digit = X%10;
+  count.push(digit);
+  ret = ret*10 + digit;
+  x = paseInt(x/10);
+}
+console.log(count.join(""));
+console.log(ret);
